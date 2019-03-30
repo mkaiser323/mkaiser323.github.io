6da34768db857d7c2b035b19a992815900abdf7f
mkaisere323 <mkaiser323@gmail.com>
2019-03-30 16:55:24 -0400

    lots of changes

--- js/json.js
+++ js/json.js
@@ -176,12 +176,12 @@ var experience = [
     "position": "Software Engineer",
     "date": "September 2018 - March 2019",
     "place": "Bombfell (Startup)",
-    "details": "Worked closely with CTO, product manager, and 4 person dev team to perform all software development for the company. Worked with stakeholders to automate and optimize business processes. Performed all roles in development and took ownership of several projects.",
+    "details": "Worked closely with CTO, product manager, and 4 person dev team to perform all software development for the company. Worked with stakeholders to automate and optimize business processes. Took ownership of multiple projects with direct impact on the business.",
     "sectioned": true,
     "sections":[
       {
        "title":"Payment System Rewrite",
-       "date":"September - March 2019",
+       "date":"Sept 2018 - Mar 2019",
        "bullet_points":[
         {"bullet":"Saved the company an average of 10% per refund transaction by fixing bug in refund process"},
         {"bullet":"Refactored payment system for improved code modularity and maintainability"},
@@ -190,7 +190,7 @@ var experience = [
       },
       {
        "title":"Pagespeed Optimization",
-       "date":"February 2019 - March 2019",
+       "date":"Feb 2019 - Mar 2019",
        "bullet_points":[
         {"bullet":"Improved load time and search ranking by increasing Google Pagespeed score by 70 points (out of 100)"},
         {"bullet":"Optimized webpack bundling using commons chunks and compression plugins"},
@@ -199,7 +199,7 @@ var experience = [
       },
       {
        "title":"Bombfell Shop Launch",
-       "date":"September 2018 - December 2018",
+       "date":"Sept 2018 - Dec 2018",
        "bullet_points":[
         {"bullet":"Coordinated with Product Manager, Stylists, and 4 other developers to launch ecommerce shop in 2 months"},
         {"bullet":"Wrote python code to search, sort, and cache shop items based on various filters and user preferences"},
@@ -208,7 +208,7 @@ var experience = [
       },
       {
        "title":"Security &amp; Authentication",
-       "date":"December 2018 - February 2019",
+       "date":"Dec 2018 - Feb 2019",
        "bullet_points":[
         {"bullet":"Implemented Multi-factor Authentication for internal accounts using totp authentication"},
         {"bullet":"Made accounts more brute-force resistant by implementing bcrypt password hashing"}
@@ -216,7 +216,7 @@ var experience = [
       },
       {
        "title":"Custom Reporting Calendar",
-       "date":"February 2019 - March 2019",
+       "date":"Feb 2019 - Mar 2019",
        "bullet_points":[
         {"bullet":"Improved business utility of reports by implementing custom reporting calendar API"},
         {"bullet":"Designed and developed custom calendar API using python and SQLAlchemy based on Nation Retail Federation calendar and internally defined reporting periods"}
@@ -230,12 +230,12 @@ var experience = [
 		"position":"Software Engineer",
 		"date":"June 2016 - September 2018",
 		"place":"General Motors",
-		"details":"Wrote software for test automation team. Received Operational Excellence Award for creating Data Setup & Diagnostics Tool.",
+		"details":"Wrote software for test automation team. Received Operational Excellence Award for creating Data Setup & Diagnostics Tool, resulting in a 30% improvement in operational efficiency.",
     "sectioned":true,
     "sections": [
         {
         "title":"Salesforce Rewrite Project",
-        "date": "January 2018 - April 2018",
+        "date": "Jan 2018 - Apr 2018",
         "bullet_points": [
           {"bullet":"Rewrote Salesforce APIs as in-house Springboot microservices to extend availability of services globally"},
           {"bullet":"Collaborated with a team of 15 developers and architects to make microservices work together seamlessly"},
@@ -244,7 +244,7 @@ var experience = [
       },
       {
         "title":"Data Setup & Diagnostics Tool",
-        "date":"February 2017 - September 2018",
+        "date":"Feb 2017 - Sept 2018",
         "bullet_points":[
         {"bullet":"Cut defect resolution and data setup times by 30% by eliminating bottlenecks in the process"},
         {"bullet":"Collaborated with a single teammate to design, develop & deploy the tool in under 6 months"},
@@ -254,7 +254,7 @@ var experience = [
       },
       {
         "title":"Test Automation",
-        "date": "Jube 2016 - September 2018",
+        "date": "Jun 2016 - Sept 2018",
         "bullet_points":[
           {"bullet":"Responsible for creating automated tests for new Salesforce REST APIs using JUnit & Jenkins"},
           {"bullet":"Improved readability and business utility of unit test results by migrating to Cucumber"},
@@ -286,7 +286,7 @@ var experience = [
 		],
 		"loc":"Bronx, NY",
 		"link":"http://climate-change-impact-studies.org"
-	},{
+	},/*{
 		"position":"Web Developer",
 		"date":"October 2015 - December 2015",
 		"place":"Electifi",
@@ -294,7 +294,7 @@ var experience = [
 		"bullet_points":[],
 		"loc":"Bronx, NY",
 		"link":"http://electifi.com"
-	},{
+	},*/{
 		"position":"Mathematics Researcher & Programmer",
 		"date":"October 2015 - January 2016",
 		"place":"CUNY - Research Foundation",
@@ -309,7 +309,7 @@ var experience = [
 		"position":"Academic Excellence Chair",
 		"date":"January 2016 - June 2016",
 		"place":"National Society of Black Engineers",
-		"details":"I organize workshops and study groups revolving around computer science classes being offered by Lehman as well as interview preparation for students that are applying for internships and jobs",
+		"details":"I organized workshops and study groups for computer science classes being offered by Lehman as well as interview preparation for students that are applying for internships and jobs",
 		"bullet_points":[],
 		"loc":"Bronx, NY",
 		"link":""
@@ -343,7 +343,7 @@ var experience = [
 var projects = [
 	{
 		"header":"Front End Web Developer",
-		"details":"Software Development class project in which I worked with 2 other teammates on a project where we implemented design principles learned in class",
+		"details":"Software Engineering class project where we worked in teams to implement design principles learned in class",
 		"taglist": [
 			{"tag":"Agile&nbsp;Development"},
 			{"tag":"JavaScript"},
@@ -365,13 +365,13 @@ var projects = [
 				},{
 					"header":"My Contributions",
 					"p":[
-						{"text":"As the front end developer of the team, I designed the user interface. I implemented a JavaScript search function to send an AJAX request to the database, pull relevant data, and dynamically display create HTML objects and display them on the screen."},
-						{"text":"I also helped with documentation and back end."}
+						{"text":"As the front end developer of the team, I designed the user interface. I implemented a JavaScript search function to send an AJAX request to the database, pull relevant data, and dynamically create HTML objects and display them on the screen."},
+						{"text":"I also helped write documentation and implement some of the back end functionalities."}
 					]
 				},{
 					"header":"Skills Learned",
 					"p":[
-						{"text":"This was my first time working with back end. I learned how to set up a local server, write basic PHP and mySQL, and make the front end interact with the back end."},
+						{"text":"This was my first time working with the back end. I learned how to set up a local server, write basic PHP and mySQL, and make the front end interact with the back end."},
 						{"text":""}
 					]
 				}
@@ -379,7 +379,7 @@ var projects = [
 		}
 	},{
 		"header":"Front End Web Developer",
-		"details":"Won first place, competing in a team of 3 to develop a brand-new mobile-friendly web site for a local business in under a week.",
+		"details":"Won first place competing in a team of 3 to develop a brand-new mobile-friendly web site for a local business in under a week.",
 		"taglist": [
 			{"tag":"HTML5"},
 			{"tag":"CSS3"},
@@ -409,7 +409,7 @@ var projects = [
 				},{
 					"header":"Skills Learned",
 					"p":[
-						{"text":"The endless hours I spent reading and working with the code in the template taught me a lot about what can be done with css, JavaScript, media queries, good coding practices. Using a pre-made template thus proved to be a lerning experience rather than a crutch."},
+						{"text":"The endless hours I spent reading and working with the code in the template taught me a lot about what can be done with css, JavaScript, media queries, and good coding practices. Using a pre-made template thus proved to be a lerning experience rather than a crutch."},
 						{"text":"This was also my first time using an API."}
 					]
 				}
@@ -417,7 +417,7 @@ var projects = [
 		}
 	},
 	{
-		"header":"Front End Web Developer",
+		"header":"Lead Developer",
 		"details":"At a 12 hour hackathon, I learned Meteor.js and used it to create a demo web app that alerts users of nearby volunteer opportunities. Our demo won third place.",
 		"taglist": [
 			{"tag":"HTML5"},
@@ -435,13 +435,13 @@ var projects = [
 				{
 					"header":"Project Overview",
 					"p":[
-						{"text":"GoDu is an app designed to use geolocation to alert users of nearby volunteer opportunities at the push of a big green button. Its name is a call to action ('Go Do') and its simple user interface is meant to encourage users to use their free time to become more active in community service."},
+						{"text":"GoDu is an app designed to use geolocation to alert users of nearby volunteer opportunities at the push of a button. Its name is a call to action ('Go Do') and its simple user interface is meant to encourage users to use their free time to become more active in community service."},
 						{"text":""}
 					]
 				},{
 					"header":"My Contributions",
 					"p":[
-						{"text":"I was the lead developer for this project. In under 12 hours, I learned Meteor.js and wrote all of the code for the app except for the placeholder JSON data."},
+						{"text":"I was the lead developer for this project. In under 12 hours, I learned the Meteor.js framework and wrote all of the code for the app except for the placeholder JSON data."},
 						{"text":""}
 					]
 				},{
@@ -471,13 +471,13 @@ var projects = [
 				{
 					"header":"Project Overview",
 					"p":[
-						{"text":"Multiplication Blocks is meant to give children a fun way to learn their multiplication. It is an extension of my Asteroids project, which I had done for my Intro to Java class. I replaced the asteroids with blocks with numbers on them. The multiplication problem shows up in the background and you have to shoot the block with the correct answer. The game also stores the questions that you got wrong in a queue and brings those questions back every time the queue is filled."},
+						{"text":"Multiplication Blocks is meant to give children a fun way to learn their multiplication. It is an extension of my Asteroids project, which I had done for my intro Java class. I replaced the asteroids with rectangular blocks and put numbers on them. The multiplication problem shows up in the background and you have to shoot the block with the correct answer. The game also stores the questions that you got wrong in a queue and brings those questions back every time the queue is filled."},
 						{"text":""}
 					]
 				},{
 					"header":"Skills Learned",
 					"p":[
-						{"text":"As a relatively inexperienced programmer at the time, the countless hours that I spent on it made me very comfortable with the Java programming language as well as general algorithm design. I also learned to make my program organized and efficient, as I was working with over 1,600 lines of code."},
+						{"text":"As a relatively inexperienced programmer at the time, the countless hours that I spent on it made me very comfortable with the Java programming language as well as general code design. I also learned to make my program organized and efficient, as I was working with over 1,600 lines of code."},
 						{"text":""}
 					]
 				}
@@ -501,13 +501,13 @@ var projects = [
 				{
 					"header":"Project Overview",
 					"p":[
-						{"text":"This the final project for our introductory Java class. We were asked to make our own versions of the classic Asteroids Game. The minimmum requirements for the project were to draw a shapes on the screen to represent the asteroids and the ship, to move the ship in response to user input, and to shoot. I went far beyond these requirements and made a fully functional game."},
+						{"text":"This was the final project for our introductory Java class. We were asked to make our own versions of the classic Asteroids Game. The minimum requirements for the project were to draw shapes on the screen to represent the asteroids and the ship, to move the ship in response to user input, and to shoot. I went far beyond these requirements and made a fully functional game."},
 						{"text":"My Asteroids game tracks score and ship damage, and stores high scores in an external text file, sorted from highest to lowest. It also has a welcome screen, separate screens to the high score, tutorial, and main menu options."}
 					]
 				},{
 					"header":"Skills Learned",
 					"p":[
-						{"text":"Java was my first programming language and creating Asteroids helped me solidify my understanding of vital Object Oriented Programming principles such as Inheritance, Data Structures, File I/O, and Algorithms."},
+						{"text":"Java was my first programming language and creating Asteroids helped me solidify my understanding of vital Object Oriented Programming principles such as Inheritance, Data Structures, File I/O, and various algorithms."},
 						{"text":""}
 					]
 				}
