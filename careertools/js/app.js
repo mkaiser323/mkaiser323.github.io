72c64a1bd2fef7abd9d17853853789c5b145c26a
mkaisere323 <mkaiser323@gmail.com>
2020-03-17 14:51:55 -0400

    added link feild, added title, made inputs to textareas

--- careertools/js/app.js
+++ careertools/js/app.js
@@ -1,12 +1,21 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-  $scope.defaults=defaults;
+	$scope.defaults=defaults;
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+	var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+	var yyyy = today.getFullYear();
 
-today = mm + '/' + dd + '/' + yyyy;
-$scope.date = today;
+	today = mm + '/' + dd + '/' + yyyy;
+	$scope.date = today;
+
+	document.title = constructTitle($scope.company, $scope.role);
+	$scope.updateTitle = function() {
+		document.title = constructTitle($scope.company, $scope.role);
+	};
 });
+
+function constructTitle(company, role){
+	return (company ? (company + "-") : "") + (role ? (role + " ") : "") + "Cover Letter";
+}
