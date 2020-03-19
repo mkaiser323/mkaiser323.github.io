162c266fcf9e39a5afb3c8751afce6cbad97f6b0
Mahedi Kaiser <mkaiser@freewheel.tv>
2020-03-19 09:06:00 -0400

    experimenting with binding auth statechange to angularjs

--- careertools/js/app.js
+++ careertools/js/app.js
@@ -14,8 +14,50 @@ app.controller('myCtrl', function($scope) {
 	$scope.updateTitle = function() {
 		document.title = constructTitle($scope.company, $scope.role);
 	};
+
+	respondToAuthStateChange($scope);
 });
 
 function constructTitle(company, role){
 	return (company ? (company + "-") : "") + (role ? (role + " ") : "") + "Cover Letter";
+}
+
+function respondToAuthStateChange($scope){
+	//Auth State Change tracking:
+	firebase.auth().onAuthStateChanged(function(user) {
+		$scope.user=user;
+		if (user) {
+			console.log("user is signed in:", user)
+		// User is signed in.
+		var displayName = user.displayName;
+		var email = user.email;
+		var emailVerified = user.emailVerified;
+		var photoURL = user.photoURL;
+		var uid = user.uid;
+		var phoneNumber = user.phoneNumber;
+		var providerData = user.providerData;
+		//   user.getIdToken().then(function(accessToken) {
+		//     document.getElementById('sign-in-status').textContent = 'Signed in';
+		//     document.getElementById('sign-in').textContent = 'Sign out';
+		//     document.getElementById('account-details').textContent = JSON.stringify({
+		//       displayName: displayName,
+		//       email: email,
+		//       emailVerified: emailVerified,
+		//       phoneNumber: phoneNumber,
+		//       photoURL: photoURL,
+		//       uid: uid,
+		//       accessToken: accessToken,
+		//       providerData: providerData
+		//     }, null, '  ');
+		//   });
+		} else {
+			console.log("user is signed out")
+		// User is signed out.
+		//   document.getElementById('sign-in-status').textContent = 'Signed out';
+		//   document.getElementById('sign-in').textContent = 'Sign in';
+		//   document.getElementById('account-details').textContent = 'null';
+		}
+	}, function(error) {
+		console.log(error);
+	});
 }
--- careertools/js/firebase/auth.js
+++ careertools/js/firebase/auth.js
@@ -12,46 +12,11 @@ var uiConfig = {
             console.log("sign in detected!")
             console.log("authResult", authResult)
             console.log("redirectUrl", redirectUrl)
-            return true;
+            return false;//true == will redirect to given url
         },
+        signInFlow: "popup"
     }
 }
 
 ui.start('#firebaseui-auth-container', uiConfig);
 
-//Auth State Change tracking:
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        console.log("user is signed in:", user)
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var uid = user.uid;
-      var phoneNumber = user.phoneNumber;
-      var providerData = user.providerData;
-    //   user.getIdToken().then(function(accessToken) {
-    //     document.getElementById('sign-in-status').textContent = 'Signed in';
-    //     document.getElementById('sign-in').textContent = 'Sign out';
-    //     document.getElementById('account-details').textContent = JSON.stringify({
-    //       displayName: displayName,
-    //       email: email,
-    //       emailVerified: emailVerified,
-    //       phoneNumber: phoneNumber,
-    //       photoURL: photoURL,
-    //       uid: uid,
-    //       accessToken: accessToken,
-    //       providerData: providerData
-    //     }, null, '  ');
-    //   });
-    } else {
-        console.log("user is signed out")
-      // User is signed out.
-    //   document.getElementById('sign-in-status').textContent = 'Signed out';
-    //   document.getElementById('sign-in').textContent = 'Sign in';
-    //   document.getElementById('account-details').textContent = 'null';
-    }
-  }, function(error) {
-    console.log(error);
-  });
