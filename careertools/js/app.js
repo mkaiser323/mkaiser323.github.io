8dc30bd2e6deb5b9e587fa7066a2e4a7fe6362e0
Mahedi Kaiser <mkaiser@freewheel.tv>
2020-03-19 22:46:30 -0400

    added sign out

--- careertools/js/app.js
+++ careertools/js/app.js
@@ -16,82 +16,11 @@ app.controller('myCtrl', function($scope) {
 	};
 
 	initFirebaseUI()
-	// mockSignIn($scope)
+	mockSignIn($scope)
 	respondToAuthStateChange($scope)
+	$scope.signOut = signOut()
 });
 
 function constructTitle(company, role){
 	return (company ? (company + "-") : "") + (role ? (role + " ") : "") + "Cover Letter";
-}
-
-function mockSignIn($scope){
-	$scope.user={
-		displayName: "Mahedi Kaiser",
-		email: "mkaiser323@gmail.com"
-	}
-}
-
-function initFirebaseUI(){
-	// Initialize the FirebaseUI Widget using Firebase: https://firebase.google.com/docs/auth/web/firebaseui
-	var ui = new firebaseui.auth.AuthUI(firebase.auth());
-	var uiConfig = {
-		signInOptions: [
-			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-		],
-		callbacks: {
-			signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-				// User successfully signed in.
-				// Return type determines whether we continue the redirect automatically
-				// or whether we leave that to developer to handle.
-				console.log("sign in detected!")
-				console.log("authResult", authResult)
-				console.log("redirectUrl", redirectUrl)
-				return false;//true == will redirect to given url
-			},
-			signInFlow: "popup"
-		}
-	}
-	ui.start('#firebaseui-auth-container', uiConfig);
-}
-
-function respondToAuthStateChange($scope){
-	//Auth State Change tracking:
-	firebase.auth().onAuthStateChanged(function(user) {
-		$scope.user=user;
-		if (user) {
-			console.log("user is signed in:", user)
-		// User is signed in.
-		var displayName = user.displayName;
-		var email = user.email;
-		var emailVerified = user.emailVerified;
-		var photoURL = user.photoURL;
-		var uid = user.uid;
-		var phoneNumber = user.phoneNumber;
-		var providerData = user.providerData;
-		  user.getIdToken().then(function(accessToken) {
-		    document.getElementById('sign-in-status').textContent = 'Signed in';
-		    document.getElementById('sign-in').textContent = 'Sign out';
-		    document.getElementById('account-details').textContent = JSON.stringify({
-		      displayName: displayName,
-		      email: email,
-		      emailVerified: emailVerified,
-		      phoneNumber: phoneNumber,
-		      photoURL: photoURL,
-		      uid: uid,
-		      accessToken: accessToken,
-		      providerData: providerData
-		    }, null, '  ');
-		  });
-		} else {
-			console.log("user is signed out")
-			//User is signed out.
-			document.getElementById('sign-in-status').textContent = 'Signed out';
-			document.getElementById('sign-in').textContent = 'Sign in';
-			document.getElementById('account-details').textContent = 'null';
-		}
-	}, function(error) {
-		console.log(error);
-	});
-	console.log("user:",$scope.user)
-	$scope.signedIn = !!$scope.user
 }
--- careertools/js/firebase/auth.js
+++ careertools/js/firebase/auth.js
@@ -1,4 +1,64 @@
 
+function mockSignIn($scope){
+	$scope.user={
+		displayName: "Mahedi Kaiser",
+		email: "mkaiser323@gmail.com"
+	}
+}
 
+function initFirebaseUI(){
+	// Initialize the FirebaseUI Widget using Firebase: https://firebase.google.com/docs/auth/web/firebaseui
+	var ui = new firebaseui.auth.AuthUI(firebase.auth());
+	var uiConfig = {
+		signInOptions: [
+			firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		],
+		callbacks: {
+			signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+				// User successfully signed in.
+				// Return type determines whether we continue the redirect automatically
+				// or whether we leave that to developer to handle.
+				console.log("sign in detected!")
+				console.log("authResult", authResult)
+				console.log("redirectUrl", redirectUrl)
+				return false;//true == will redirect to given url
+			},
+			signInFlow: "popup"
+		}
+	}
+	ui.start('#firebaseui-auth-container', uiConfig);
+}
 
+function respondToAuthStateChange($scope){
+	//Auth State Change tracking:
+	firebase.auth().onAuthStateChanged(function(user) {
+		$scope.user=user;
+		if (user) {
+			console.log("user is signed in:", user)
+		// User is signed in.
+		var displayName = user.displayName;
+		var email = user.email;
+		var emailVerified = user.emailVerified;
+		var photoURL = user.photoURL;
+		var uid = user.uid;
+		var phoneNumber = user.phoneNumber;
+		var providerData = user.providerData;
 
+		} else {
+			console.log("user is signed out")
+			// User is signed out.
+		}
+	}, function(error) {
+		console.log(error);
+	});
+	console.log("user:",$scope.user)
+	$scope.signedIn = !!$scope.user
+}
+
+function signOut(){
+	firebase.auth().signOut().then(function() {
+		console.log('Signed Out');
+	  }, function(error) {
+		console.error('Sign Out Error', error);
+	  });
+}
