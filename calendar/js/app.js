940b386848e2bd99189fe0ddc08b2e34447bfc8d
mkaisere323 <mkaiser323@gmail.com>
2020-04-24 12:53:15 -0400

    refactored code

--- calendar/js/app.js
+++ calendar/js/app.js
@@ -1,16 +1,50 @@
 var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope, $compile) {
-	var title='calendar.pdf'
-	var htmlString = 'Hello World'
 
-	appendHtmlByID(htmlString, '#calendar')
-	saveToPdf(htmlString, title)
+class Calendar{
+	constructor(title, body) {
+    	this.title = title;
+    	this.body = body;
+    	this.ext = 'pdf';
+  	}
+
+  	get fileName(){
+  		return this.title + '.' + this.ext
+  	}
+}
+
+var save = true;
+app.controller('myCtrl', function($scope) {
+	calendar = generateCalendar()
+	console.log(calendar)
+
+	appendHtmlByID(calendar.body, '#calendar')
+
+	if (save){
+		saveToPdf(calendar.body, calendar.fileName)
+	}
 });
 
-function saveToPdf(content, title){
-	var doc = new jsPDF()
+
+/*
+Try this:
+https://itnext.io/javascript-convert-html-css-to-pdf-print-supported-very-sharp-and-not-blurry-c5ffe441eb5e
+https://html2canvas.hertzen.com/ => https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js
+*/
+
+
+function generateCalendar(){
+	var title = "April 2020"
+	var body = "<h1>" + title + "</h1>";
+
+	return new Calendar(title, body)
+}
+
+function saveToPdf(content, fileName){
+	var doc = new jsPDF({
+		orientation: 'landscape',
+	})
 	doc.text(content, 10, 10)
-	doc.save(title)
+	doc.save(fileName)
 }
 
 
