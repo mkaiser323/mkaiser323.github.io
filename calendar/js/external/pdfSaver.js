dca7f4cc88e64813ac4eaa09761546d0726c555d
Mahedi Kaiser <mkaiser@freewheel.tv>
2020-08-02 03:14:14 -0400

    added better way to create landscape pdf

--- calendar/js/external/pdfSaver.js
+++ calendar/js/external/pdfSaver.js
@@ -1,19 +1,39 @@
+//credit: https://stackoverflow.com/a/37966002
+function rotateCanvas(canvas){
+	  // create intermediate canvas
+	  var rotCanvas = document.createElement("canvas");
+  
+	  // swap width and height
+	  rotCanvas.width = canvas.height;
+	  rotCanvas.height = canvas.width;
+  
+	  // get context
+	  var rctx = rotCanvas.getContext("2d");
+  
+	  // translate to center (rotation pivot)
+	  rctx.translate(rotCanvas.width * 0.5, rotCanvas.height * 0.5);
+  
+	  // rotate -90° (CCW)
+	  rctx.rotate(-Math.PI * 0.5);
+  
+	  // draw image offset so center of image is on top of pivot
+	  rctx.drawImage(canvas, -canvas.width * 0.5, -canvas.height * 0.5);
+	  
+	  return rotCanvas
+}
+
 function saveSelectionAsPDF(filename, selector, orientation){
 	html2canvas(document.querySelector(selector)).then(function(canvas) {
-		saveImageAsPDF(canvas.toDataURL('image/png'), 792, 612, filename, orientation)
+		if (orientation == 'landscape'){
+			canvas = rotateCanvas(canvas)
+		}
+		saveImageAsPDF(canvas, 792, 612, filename, orientation)
 	});
 }
 
-function saveImageAsPDF(imageData, long, short, fileName, orientation){
-	if (orientation == 'landscape'){
-		width = long;
-		height = short;
-	} else {
-		width = short;
-		height = long;
-	}
+function saveImageAsPDF(imageData, height, width, fileName, orientation){
 	var doc = new jsPDF({
-		orientation: orientation,
+		orientation: 'portrait',
 		unit: 'pt',
 		format: 'letter'
 	})
